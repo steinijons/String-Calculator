765f80043d13d25ec067c8bde2c5957d34f5e501
Steingrímur Arnar Jónsson <Steingrmur Arnar Jnsson>
2016-10-12 11:10:06 +0000

    sett meyra í calculator.java og calculatortest

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,17 +1,34 @@
-package main;
+package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String test)
-	{
-		return 0;
+	public static int add(String text){
+		if(text.equals("")){
+			return 0;
+		}
+		else if(text.contains(",")){
+			return sum(splitNumbers(text));
+		}
+		else
+			return 1;
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	private static int toInt(String number){
+		return Integer.parseInt(number);
 	}
-	
-	
+
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+	}
+      
+    private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
+
+
 
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -1,17 +1,32 @@
-package main;
+package is.ru.stringcalculator;
 
-public class Calculator {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-	public static int add(String test)
-	{
-		return 0;
+public class CalculatorTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    }
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
 	}
-	
-	
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}	
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
 
 }
