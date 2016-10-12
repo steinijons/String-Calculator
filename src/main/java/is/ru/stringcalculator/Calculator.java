1b52f5255d5ce42857c582f566898ceaba5c9051
Steingrímur Arnar Jónsson <Steingrmur Arnar Jnsson>
2016-10-12 13:14:02 +0000

    Grunnur frá Hannesi

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,12 +2,31 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 
-	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else
-			return 1;
+public class CalculatorTest {
+
+	public static void main(String args[]) {
+      org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+    }
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
 	}
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}	
+
+	@Test
+    public void testMultipleNumbers(){
+    	assertEquals(6, Calculator.add("1,2,3"));
+    }
+
+}
 }
