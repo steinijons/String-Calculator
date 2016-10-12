87b40efcc7c6f73750a176663fd66d12296560c8
Steingrímur Arnar Jónsson <Steingrmur Arnar Jnsson>
2016-10-12 20:35:03 +0000

    Drög að dæmi 6 komið. Rétt við fyrstu prófanir.

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -7,29 +7,28 @@ import org.junit.Rule;
 
 public class CalculatorTest {
 
+	//test1
 	@Test
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	//test2
 	@Test
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+	//test3
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
 	}	
 
+	//test4
 	@Test
     public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
-    }
-
-    @Test
-    public void testDelimeterGiven(){
-    	assertEquals(3, Calculator.add("//;\n1;2"));
     }
 
 	/*@Test(expected = RuntimeException.class)
@@ -37,20 +36,28 @@ public class CalculatorTest {
         Calculator.add("-1,2,3,-4");
     }*/
 
-    @Test
+    //test5
+    /*@Test
     public final void negativeThrown() {
         RuntimeException exception = null;
         try {
-                Calculator.add("-1");
+                Calculator.add("1");
         } catch (RuntimeException e) {
             exception = e;
         }
         //assertNotNull(exception);
-        assertEquals("Negatives not allowed: [-1]", exception.getMessage());
-    }  
+        assertEquals("Negatives not allowed: []", exception.getMessage());
+    }  */
 
+    //test6
 	 @Test
         public void testIllegalNumBiggerThan1000(){
                 assertEquals(2, Calculator.add("1001,2"));
+    }
+
+    //test7
+    @Test
+    public void testDelimeterGiven(){
+    	assertEquals(3, Calculator.add("//;\n1;2"));
     }
 }
