552b5230c6b1150eb17c4c35fc0f84f1d4c74eaa
Steingrímur Arnar Jónsson <Steingrmur Arnar Jnsson>
2016-10-12 13:01:11 +0000

    1. testcase virkar

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,26 +6,8 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
-			return sum(splitNumbers(text));
-		}
 		else
 			return 1;
 	}
 
-	private static int toInt(String number){
-		return Integer.parseInt(number);
-	}
-
-	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
-	}
-      
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
-		}
-		return total;
-    }
 }
