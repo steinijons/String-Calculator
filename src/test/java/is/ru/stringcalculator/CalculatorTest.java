b726145436a3648c33490d8fe64f61b89ed31b54
Steingrímur Arnar Jónsson <Steingrmur Arnar Jnsson>
2016-10-12 13:04:01 +0000

    testcase f/1. lid kominn

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -10,4 +10,13 @@ public class CalculatorTest {
 		assertEquals(0, Calculator.add(""));
 	}
 
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+	}
+
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
 }
